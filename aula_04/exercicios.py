fc99dabadc51fc8fa7bbdf707efcb81bf5bf4ad9
Julio Pereira <juliopereira.dev@gmail.com>
2025-05-16 18:37:47 -0300

    Inicio aula 04

---
+++ aula_04/exercicios.py
@@ -0,0 +1,33 @@
+"""
+    Laboratório 1
+    Criar um programa que utilize uma estrutura de repetição for. Nesta estrutura, o usuário deverá fornecer 5 nomes
+    a serem adicionados em uma lista. No final, apresentar os nomes em ordem crescente.
+    
+    => O que vai aplicar neste exercício:
+        - list
+        - Input()
+        - for
+        - print()
+
+"""
+
+"""
+
+    Laboratório 2
+    Neste laboratório, uma lista de 100 números será criada de forma aleatória, ou seja, os elementos serão números
+    aleatórios.
+    Escrever o programa de forma a exibir E adicionar em uma lista, apenas os valores gerados que sejam maiores que
+    10.
+
+        => O que vai aplicar neste exercício:
+            - random (from random import randint)
+            - list
+            - for
+            - print()
+
+"""
+
+from random import randint
+
+
+numero = randint(100)
