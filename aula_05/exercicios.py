6aaace665189a29cddaa67ca944aae1314dd8c10
Julio Pereira <juliopereira.dev@gmail.com>
2025-05-19 18:36:13 -0300

    Inicio aula 05

---
+++ aula_05/exercicios.py
@@ -0,0 +1,20 @@
+"""
+    Laboratório 01
+
+    Este programa representa um jogo para adivinhar números. Os passos ssão apresentados a seguir, com um 
+    exemplo de uso:
+
+        => O programa produz um número aleatório inteiro entre 1 e 100 (suponha: 49);
+        => O programa pede para o usuário: "Informe um valor entre 0 e 100": 60;
+        => Como o número informado é maior que o produzido, o programe pede: "Informe um valor entre 0 e 59" : 20;
+        => Como o número informado é menor que o produzido, o programa pede: "Informe um valor entre 21 e 59";
+        => No final, o programa apresenta o número de tentativas.
+
+    Observação: se o usuário fornece um valor fora da faixa solicitada, conta como tentativa.
+
+"""
+
+# MÓDULOS
+# LOOP DE REPETIÇÃO
+# ENTRADA E SAÍDA DE DADOS
+# OPERADORES
