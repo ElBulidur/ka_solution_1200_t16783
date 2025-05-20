db81b6b6d511420c040e44484e04bca0a61c4f23
Julio Pereira <juliopereira.dev@gmail.com>
2025-05-20 18:39:26 -0300

    Inicio aula 06

---
+++ aula_06/exercicios.py
@@ -0,0 +1,10 @@
+"""
+    Laboratório 1
+
+    Tomar como base o cálculo do Imposto de Renda Pessoa Física. O acesso para este cálculo pode ser realizado
+    através do link: https://www27.receita.fazenda.gov.br/simulador-irpf/
+    Escrever uma função que receba como parâmetro o valor da base de cálculo do salário, e retorne o imposto de
+    renda correspondente.
+    Sugestão: Usar tuplas para armazenar as faixas de valores.
+    
+"""
